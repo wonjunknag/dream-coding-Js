bb6a1847f7168615e3b400b569719f9c98eeac55
wonjunkang <kwjcomein@gmail.com>
2020-07-23 16:40:08 +0900

    javascriptQuiz-update

--- 12.Quiz/quiz.js
+++ 12.Quiz/quiz.js
@@ -74,4 +74,111 @@ function staring(number) {
 // const num = prompt();
 // staring(num);
 
-// quiz11.
+// quiz11. add for
+let s = 0;
+for (let i = 0; i < 101; i++) {
+  s = s + i;
+}
+console.log(s);
+
+// quiz12. game character
+class Wizard {
+  constructor(heath, mana, armor) {
+    this.heath = heath;
+    this.mana = mana;
+    this.armor = armor;
+  }
+
+  attack() {
+    console.log("파이어볼");
+  }
+}
+
+const x = new Wizard(545, 210, 10);
+console.log(x.heath, x.mana, x.armor);
+x.attack();
+
+// 545 210 10
+// 파이어볼
+
+// quiz13
+const planet = {
+  1: "수성",
+  2: "금성",
+  3: "지구",
+  4: "화성",
+  5: "목성",
+  6: "토성",
+  7: "천왕성",
+  9: "해왕성",
+};
+
+// const planetName = prompt();
+// console.log(planet[planetName]);
+
+// quiz14.
+function multiple() {
+  // let input = prompt();
+  if (input % 3 === 0) {
+    console.log("짝");
+  } else {
+    console.log(input);
+  }
+}
+// multiple();
+
+// quiz15
+function aboutMe() {
+  // let name = prompt();
+  console.log(`안녕하세요, 저는 ${name}입니다.`);
+}
+// aboutMe();
+
+// quiz16
+{
+  let word = "거꾸로".split("");
+  console.log(word.reverse().join(""));
+}
+
+// quiz17
+{
+  // const stature = prompt();
+  function statureLimit(stature) {
+    if (stature < 150) {
+      console.log("NO");
+    } else {
+      console.log("YES");
+    }
+  }
+  // statureLimit(stature);
+}
+
+// quiz18
+{
+  const subject = { math: 20, english: 30, korean: 40 };
+  function sujectAvg(subject) {
+    const sum = subject.math + subject.english + subject.korean;
+    const result = sum / 3;
+    console.log(result);
+  }
+  sujectAvg(subject);
+}
+
+// quiz19
+{
+  const number = "2 3";
+  const numberArray = number.split(" ");
+  console.log(parseInt(numberArray[0]) ** numberArray[1]);
+}
+
+// quzi20
+{
+  const number = "10 5";
+  const numberArray = number.split(" ");
+  console.log(
+    parseInt(numberArray[0] / numberArray[1]),
+    parseInt(numberArray[0] % numberArray[1])
+  );
+}
+
+// quiz21
